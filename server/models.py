bfba9c2aba0b50c8edb6c92f88c21f3919f59313
+ <+>
2023-10-30 23:58:12 -0400

    changed models structure

--- server/models.py
+++ server/models.py
@@ -4,64 +4,6 @@ from config import db, bcrypt
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
 
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable = False)
-    email = db.Column(db.String)
-    image = db.Column(db.String)
-    _password_hash = db.Column(db.String)
-
-    reviews = db.relationship('Review', back_populates='user')
-    products = db.relationship('Product', secondary='review', back_populates='users')
-
-    @hybrid_property
-    def password_hash(self):
-        raise AttributeError('Password hashes may not be viewed.')
-    
-    @password_hash.setter
-    def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
-        self._password_hash = password_hash.decode('utf-8')
-
-    def authenticate(self, password):
-        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
-
-    @validates("email")
-    def validate_email(self, key, email):
-        if "@" not in email:
-            raise ValueError("Please provide a valid email")
-        return email
-
-
-    def __repr__(self):
-        return f'<User username is {self.username}>'
-    
-    serialize_rules = (
-        '-_password_hash',
-    )
-
-
-class Product(db.Model, SerializerMixin):
-    __tablename__ = 'products'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
-    ecoFriendlyFeatures = db.Column(db.String)
-    category = db.Column(db.String)
-
-    manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturers.id'))
-    reviews = db.relationship('Review', back_populates='product')
-    users = db.relationship('User', secondary='review', back_populates='products')
-
-
-    def __repr__(self):
-        return f'<Product name is {self.name}>'
-    
-
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
@@ -78,13 +20,67 @@ class Review(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'<Review content: {self.content}>'
-    
-    
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    image = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    reviews = db.relationship('Review', back_populates='user')
+    products = db.relationship('Product', secondary='reviews', back_populates='users', viewonly=True)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates("email")
+    def validate_email(self, key, email):
+        if "@" not in email:
+            raise ValueError("Please provide a valid email")
+        return email
+
+    def __repr__(self):
+        return f'<User username is {self.username}>'
+
+    serialize_rules = (
+        '-_password_hash',
+    )
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    ecoFriendlyFeatures = db.Column(db.String)
+    category = db.Column(db.String)
+
+    manufacturer_id = db.Column(db.Integer, db.ForeignKey('manufacturers.id'))
+    manufacturer = db.relationship('Manufacturer', back_populates='products')
+
+    reviews = db.relationship('Review', back_populates='product', viewonly=True)
+    users = db.relationship('User', secondary='reviews', back_populates='products', viewonly=True)
+
+    def __repr__(self):
+        return f'<Product name is {self.name}'
+
 class Manufacturer(db.Model, SerializerMixin):
     __tablename__ = 'manufacturers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    
-    products = db.relationship('Product', back_populates = 'manufacturer')
+
+    products = db.relationship('Product', back_populates='manufacturer')
--- server/seed.py
+++ server/seed.py
@@ -1,22 +1,69 @@
-from random import randint
+from random import choice as rc
+from faker import Faker
 
 from app import app
-from models.models import User
-from config import db
-
-def seed():
-    # delete current data
-    User.query.delete()
-    
-    # create admin user for testing and debugging
-    admin = User(username="admin", admin=True)
-    admin.password_hash = 'admin'
-
-    db.session.add(admin)
-    db.session.commit()
+from config import db, bcrypt
+from models import User, Review, Manufacturer, Product
 
 if __name__ == '__main__':
+    fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        seed()
-        print("Seed complete!")
+        print("Deleting all records...")
+        User.query.delete()
+        Review.query.delete()
+        Manufacturer.query.delete()
+        Product.query.delete()
+
+        users = []
+        products = []
+        manufacturers = []
+        reviews = []
+
+        # Create Users
+        for _ in range(10):
+            user = User(
+                username=fake.user_name(),
+                email=fake.email(),
+                image=fake.image_url(),
+                _password_hash=bcrypt.generate_password_hash('password').decode('utf-8')
+            )
+            users.append(user)
+
+        db.session.add_all(users)
+
+        # Create Manufacturers
+        for _ in range(5):
+            manufacturer = Manufacturer(
+                name=fake.company(),
+                description=fake.sentence()
+            )
+            manufacturers.append(manufacturer)
+
+        db.session.add_all(manufacturers)
+
+        # Create Products
+        for _ in range(20):
+            product = Product(
+                name=fake.word(),
+                description=fake.sentence(),
+                ecoFriendlyFeatures=fake.word(),
+                category=fake.word(),
+                manufacturer=rc(manufacturers)
+            )
+            products.append(product)
+
+        db.session.add_all(products)
+
+        # Create Reviews
+        for _ in range(30):
+            review = Review(
+                rating=rc([1, 2, 3, 4, 5]),
+                content=fake.paragraph(),
+                user=rc(users),
+                product=rc(products)
+            )
+            reviews.append(review)
+
+        db.session.add_all(reviews)
+
+        db.session.commit()
